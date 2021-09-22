8fa6956f36278bee9ff55aa52914ca0b47166883
Anup Kumar Shukl <86565390+anupshukla276@users.noreply.github.com>
2021-09-22 13:59:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
-const hamburgar = document.querySelector('.hamburgar')
-const navbar = document.querySelector('.navbar');
+const handleMenuOpacityDesktopVersion = () => {
+    let width = document.body.offsetWidth;
+    if(width >= 784 && document.querySelector(".opacityCount").innerHTML === "0") {
+        document.querySelector(".menu").style.opacity = "1";
+    }
+    if(width <= 784 && document.querySelector(".opacityCount").innerHTML === "0") {
+        document.querySelector(".menu").style.opacity = "0";
+    }
+}
 
-hamburgar.addEventListener('click', () => {
-    navbar.classList.toggle('show');
-})
+const handleMenuClick = () => {
+    let menu = document.querySelector("header .headerWrapper .menu");
+    let dropdownArrow = document.querySelector(".dropdownArrow");
+    let opacityCount = document.querySelector(".opacityCount");
+
+    if(opacityCount.innerHTML === "0") {
+        menu.style.opacity = "1";
+        dropdownArrow.style.opacity = "1";
+        opacityCount.innerHTML = "1";
+    }
+    else {
+        menu.style.opacity = "0";
+        dropdownArrow.style.opacity = "0";
+        opacityCount.innerHTML = "0";
+    }
+
+    console.log(document.body.offsetWidth);
+}
